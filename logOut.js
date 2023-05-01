575e16649d15bed25741700b653db14b4b657093
Dani Funes <funesfuentesdaniel@gmail.com>
2023-05-01 21:24:40 +0200

    feat: auth login && modify logout

--- logOut.js
+++ logOut.js
@@ -1,13 +1,11 @@
 const myButton = document.querySelector(".btnOut")
 myButton.addEventListener("click", function() {
-    // Recorre JSON
-    const usersObject = JSON.parse(localStorage.getItem("users"))
-    usersObject.forEach((user) => {
-      // Verifica si es false y si lo es cambia a true
-      if (user.usuario.isLogin === true) {
-        user.usuario.isLogin = false ;
-        localStorage.setItem("users", JSON.stringify(usersObject))
-        window.location.replace("index.html")
-      } 
-    });
+    const usersObject = JSON.parse(localStorage.getItem("activeUser"))
+    
+    if (usersObject.auth === true) {
+      usersObject.auth = false ;
+      localStorage.setItem("activeUser", JSON.stringify(usersObject))
+      window.location.replace("index.html")
+    } 
+   
   });
